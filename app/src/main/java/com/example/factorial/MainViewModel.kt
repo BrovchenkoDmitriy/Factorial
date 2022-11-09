b01c05de0cc1684e9cb3d97affe4a4a0f040cb65
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2022-11-09 12:12:25 +0900

    create fun factorial using withContext(Dispatchers...){} or suspendCoroutine{}

--- app/src/main/java/com/example/factorial/MainViewModel.kt
+++ app/src/main/java/com/example/factorial/MainViewModel.kt
@@ -4,8 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
 
 
 class MainViewModel : ViewModel() {
@@ -19,12 +24,31 @@ class MainViewModel : ViewModel() {
             _state.value = Error
             return
         }
-        viewModelScope.launch{
+        viewModelScope.launch {
             val number = value.toLong()
-            //calculate
-            delay(1000)
-            _state.value = Factorial(number.toString())
+            _state.value = Factorial(factorial(number))
         }
+    }
 
+//    private suspend fun factorial(value: Long):String{
+//        return withContext(Dispatchers.Default){
+//            var result = BigInteger.ONE
+//            for (i in 1..value){
+//                result = result.multiply(BigInteger.valueOf(i))
+//            }
+//           result.toString()
+//        }
+//    }
+
+    private suspend fun factorial(value: Long): String {
+        return suspendCoroutine {
+            thread {
+                var result = BigInteger.ONE
+                for (i in 1..value) {
+                    result = result.multiply(BigInteger.valueOf(i))
+                }
+                it.resumeWith(Result.success(result.toString()))
+            }
+        }
     }
 }
